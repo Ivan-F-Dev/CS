307e3fde462f3c2b7d4fcbf44ac29e1906d61725
Ivan-F-Dev <89821080113@mail.ru>
2022-10-22 22:16:57 +0400

    second commit

---
+++ client/src/components/Categories/Categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Categories = (props) => {
+
+
+    return (
+        <div>
+            Categories
+        </div>
+    );
+
+
+
+}
+
+export default Categories;
